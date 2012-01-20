8700be8059dde2c509d2397a3e868602455b5501
Michele Pratusevich <michele.pratusevich@gmail.com>
2012-01-20 13:28:09 -0500

    Second commit

---
+++ Kinectophone/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.Kinect.Nui;
+using Microsoft.Research.Kinect.Audio;
+
+
+namespace Kinectophone
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Window_Loaded(object sender, EventArgs e)
+        {
+            Runtime nui = Runtime.Kinects[0];
+            nui.Initialize(RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+
+            nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
+            nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+        }
+
+        void nui_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            kinectColorOut.
+        }
+    }
+}
