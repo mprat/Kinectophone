5f1229122e93f5867d630cffc5b375b6a292af92
Michele Pratusevich <michele.pratusevich@gmail.com>
2012-01-20 14:23:20 -0500

    Head tracked and followed

--- Kinectophone/MainWindow.xaml.cs
+++ Kinectophone/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Research.Kinect.Nui;
 using Microsoft.Research.Kinect.Audio;
+using Coding4Fun.Kinect.Wpf;
 
 
 namespace Kinectophone
@@ -22,23 +23,45 @@ namespace Kinectophone
     /// </summary>
     public partial class MainWindow : Window
     {
+        Runtime nui = Runtime.Kinects[0];
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public void Window_Loaded(object sender, EventArgs e)
+        private void Window_Loaded(object sender, EventArgs e)
         {
-            Runtime nui = Runtime.Kinects[0];
             nui.Initialize(RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
 
             nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
             nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+
+            nui.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            nui.Uninitialize();
+        }
+
+        void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            SkeletonData skeleton = (from s in e.SkeletonFrame.Skeletons
+                                     where s.TrackingState == SkeletonTrackingState.Tracked
+                                     select s).FirstOrDefault();
+
+            if (skeleton.TrackingState == SkeletonTrackingState.Tracked)
+            {
+                Joint head = skeleton.Joints[JointID.Head].ScaleTo((int)canvas1.Height, (int)canvas1.Width, .5f, .5f);
+                Canvas.SetLeft(headEllipse, head.Position.X);
+                Canvas.SetTop(headEllipse, head.Position.Y);
+            }
         }
 
         void nui_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
         {
-            kinectColorOut.
+            kinectColorOut.Source = e.ImageFrame.ToBitmapSource();
         }
     }
 }
